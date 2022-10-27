45592bd5ed583bf2119a6f6f7c9c5385494e5812
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-27 14:47:29 +0530

    product card styling

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,7 +2,9 @@
   <div class="col-3 mt-3">
     <div class="card h-100 text-left">
       <!-- <img class="w-100" src="https://via.placeholder.com/150x100"  /> -->
-      <img class="w-100" :src="product.image" alt />
+      <div class="product_img">
+        <img class="img" :src="product.image" alt />
+      </div>
       <div class="card-body">
         <h4 class="card-title">
           <router-link :to="{ name: 'product', params: { id: product.id } }">{{
@@ -26,5 +28,14 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.product_img {
+  margin: auto;
+  height: 250px;
+}
+.product_img .img {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+}
 </style>
