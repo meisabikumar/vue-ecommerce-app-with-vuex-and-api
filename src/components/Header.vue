65efaddc1486ecbf79b618db7bbb95dabeabb11a
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-27 13:03:24 +0530

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <div class="container">
+        <div class="nav navbar-nav">
+          <router-link to="/" class="nav-tem nav-link active">Home</router-link>
+          <a class="nav-item nav-link" href="#">Product</a>
+        </div>
+
+        <div>
+          <div class="dropdown open">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="triggerId"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              0 Cart
+            </button>
+            <div>
+              <mini-cart />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+  
+  <script>
+import MiniCart from "./MiniCart";
+
+export default {
+  components: { MiniCart },
+};
+</script>
+  
+  <style>
+</style>
---
+++ src/components/MiniCart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="dropdown-menu p-2"
+    style="min-width: 320px; right: 0; left: auto"
+    aria-labelledby="triggerId"
+  >
+    <div>
+      <div class="px-2 d-flex justify-content-between">
+        <div>
+          <strong>Product 1</strong>
+          <br />
+          1 x 100
+        </div>
+        <div>
+          <a href="#" class="badge bg-secondary">remove</a>
+        </div>
+      </div>
+      <hr />
+    </div>
+
+    <div class="d-flex justify-content-between">
+      <span>Total: Rs. 0</span>
+      <a href="#">Clear Cart</a>
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "MiniCart",
+};
+</script>
+  
+  <style>
+</style>
